0d6843d2abfb5fc7433492ac3b5360358748aff0
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2024-05-06 11:12:16 +0200

    lagt till routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
+/* include components */
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MyCoursesComponent } from './my-courses/my-courses.component';
 
-export const routes: Routes = [];
+/* routes */
+export const routes: Routes = [
+    {path: "home", component: HomeComponent},
+    {path: "mycourses", component: MyCoursesComponent},
+    {path: "", redirectTo: "/home", pathMatch: "full"},
+    {path: "**",  component: HomeComponent}
+];
