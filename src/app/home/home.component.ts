a4d95c2e9f79b7923a804973b4c8aad09a9650b6
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2024-05-07 18:05:57 +0200

    lagt till select med ämnen och funktion för filtrering av ämnen

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,13 +12,36 @@ import { CommonModule } from '@angular/common';
 })
 export class HomeComponent {
 
-  courselist: CourseInterface[] = [];
+  courselist: CourseInterface[] = [];//Array to store list with courses
+  uniqueSubjects: string[] = [];//Array to store uniqu subjects
 
-  constructor(private coursedataservice: CoursedataService) {}
+  constructor(private coursedataservice: CoursedataService) { }
 
+  //subscribe to the getCourses method
   ngOnInit() {
     this.coursedataservice.getCourses().subscribe(data => {
       this.courselist = data;
+
+      this.extractUniqueSubjects();//calls function to extract unique subjects
     })
+  }
+
+  //function to extract unique subjects from courses
+  extractUniqueSubjects(): void {
+    this.uniqueSubjects = Array.from(new Set(this.courselist.map(course => course.subject)));
+  }
+
+  //function to filter courses from selected subject
+  filterCoursesBySubject(event: any): void {
+    const selectedSubject = event.target.value;
+    if (!selectedSubject) {
+      this.coursedataservice.getCourses().subscribe(data => {
+        this.courselist = data;
+      });
+    } else {
+      this.coursedataservice.getCourses().subscribe(data => {
+        this.courselist = data.filter(course => course.subject === selectedSubject);
+      });
+    }
   }
 }
