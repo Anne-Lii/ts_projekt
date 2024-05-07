3973393e8e793ca3b8250c526ea2a8a794e3c294
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2024-05-07 16:41:12 +0200

    gjort http anrop med httpclient i service coursedata och fått ut data till skärmen till ett table

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { CourseInterface } from '../model/course-interface';
+import { CoursedataService } from '../services/coursedata.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
+  courselist: CourseInterface[] = [];
+
+  constructor(private coursedataservice: CoursedataService) {}
+
+  ngOnInit() {
+    this.coursedataservice.getCourses().subscribe(data => {
+      this.courselist = data;
+    })
+  }
 }
--- src/app/services/coursedata.service.ts
+++ src/app/services/coursedata.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CourseInterface } from '../model/course-interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursedataService {
   private url:string = "https://matdah.github.io/DT208G---Programmering-i-TypeScript/Moment%205%20-%20Projekt/miun_courses.json";
-  constructor() { }
+  
+  //dependencie injection http client
+  constructor(private http: HttpClient) { } 
+  
+  //method to get courses with http client
+  getCourses(): Observable<CourseInterface[]> {
+    return this.http.get<CourseInterface[]>(this.url);//http call
+  }
 }
