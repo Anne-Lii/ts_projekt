3d3df15827022b38cf40d0fd9f9b73cce7553b1c
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2024-05-07 20:34:06 +0200

    lagt till sökfunktion i searchinput

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,19 +2,23 @@ import { Component } from '@angular/core';
 import { CourseInterface } from '../model/course-interface';
 import { CoursedataService } from '../services/coursedata.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
+  originalCourselist: CourseInterface[] = [];//store the original list of courses
   courselist: CourseInterface[] = [];//Array to store list with courses
-  uniqueSubjects: string[] = [];//Array to store uniqu subjects
-  filteredCount: number = 0;//filteredCount set to 0
+  uniqueSubjects: string[] = [];//Array to store unique subjects
+  filteredCount: number = 0;//number of courses set to 0
+  searchInput: string = "";//empty string for searchinput
+
 
   constructor(private coursedataservice: CoursedataService) { }
 
@@ -22,7 +26,7 @@ export class HomeComponent {
   ngOnInit() {
     this.coursedataservice.getCourses().subscribe(data => {
       this.courselist = data;
-
+      this.originalCourselist = [...this.courselist];
       this.extractUniqueSubjects();//calls function to extract unique subjects
       this.filteredCount = this.courselist.length;//Initially set filteredCount to total number of courses
     })
@@ -47,5 +51,26 @@ export class HomeComponent {
         this.filteredCount = this.courselist.length; //Update filteredCount to number of filtered courses
       });
     }
+  }
+
+  //function to search from searchinput
+  searchCourses(): void {
+    if (this.searchInput.trim() === "") {
+      this.resetCourses();
+      return;
+    }
+
+    this.courselist = this.originalCourselist.filter(course =>
+      course.courseCode.toLowerCase().includes(this.searchInput.toLowerCase()) ||
+      course.courseName.toLowerCase().includes(this.searchInput.toLowerCase()) ||
+      course.subject.toLowerCase().includes(this.searchInput.toLowerCase())
+    );
+    this.filteredCount = this.courselist.length;
+  }
+
+  // Function to reset courselist to its original state
+  resetCourses(): void {
+    this.courselist = [...this.originalCourselist];
+    this.filteredCount = this.courselist.length;
   }
 }
