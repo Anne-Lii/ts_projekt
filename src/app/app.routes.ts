e25bfaf7909b6c40b612413697ef6b5f65fc4fbf
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2024-05-14 15:23:28 +0200

    lagt till en sida som startsida med fiktiv info om universitetet mm och stylat

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MyCoursesComponent } from './my-courses/my-courses.component';
+import { CourselistComponent } from './courselist/courselist.component';
 
 /* routes */
 export const routes: Routes = [
     {path: "home", component: HomeComponent},
+    {path: "courselist", component: CourselistComponent},
     {path: "mycourses", component: MyCoursesComponent},
     {path: "", redirectTo: "/home", pathMatch: "full"},
     {path: "**",  component: HomeComponent}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,7 @@ import { FormsModule } from '@angular/forms';
 import { TimetableService } from '../services/timetable.service';
 import { NgxPaginationModule } from 'ngx-pagination';
 
- 
+
 
 @Component({
   selector: 'app-home',
@@ -18,101 +18,9 @@ import { NgxPaginationModule } from 'ngx-pagination';
 })
 export class HomeComponent {
 
-  originalCourselist: CourseInterface[] = [];//store the original list of courses
-  courselist: CourseInterface[] = [];//Array to store list with courses
-  uniqueSubjects: string[] = [];//Array to store unique subjects
-  filteredCount: number = 0;//number of courses set to 0
-  searchInput: string = "";//empty string for searchinput
-  currentSortColumn: keyof CourseInterface = "courseCode"; //starting with courseCode sorted
-  isSortAscending: boolean = true;//sortingdirection  
-  totalLength: number = 0;
-  page: number = 1;//start on page one
 
+  constructor() { }
 
-  constructor(
-    private coursedataservice: CoursedataService, //inject coursedataservie
-    private timeTableService: TimetableService, //inject timetableservice
-
-  ) { }
-
-  //subscribe to the getCourses method
-  ngOnInit() {
-    this.coursedataservice.getCourses().subscribe(data => {
-      this.originalCourselist = data; // Initialize originalCourselist
-      this.updateDisplayedCourses();
-    });
-  }
-
-
-
-  filterCoursesBySubject(event: any): void {
-    const selectedSubject = event.target.value;
-    if (!selectedSubject || selectedSubject === "all") {
-      this.resetCourses();
-    } else {
-      this.courselist = this.originalCourselist.filter(course => course.subject === selectedSubject);
-      this.filteredCount = this.courselist.length;
-    }
-}
-
-
-
-  //function to search from searchinput
-  searchCourses(): void {
-    if (this.searchInput.trim() === "") {
-      this.resetCourses();
-      return;
-    }
-
-    this.courselist = this.originalCourselist.filter(course =>
-      course.courseCode.toLowerCase().includes(this.searchInput.toLowerCase()) ||
-      course.courseName.toLowerCase().includes(this.searchInput.toLowerCase()) ||
-      course.subject.toLowerCase().includes(this.searchInput.toLowerCase())
-    );
-    this.filteredCount = this.courselist.length;
-    this.extractUniqueSubjects();
-  }
-
-  // Function to reset courselist to its original state
-  resetCourses(): void {
-    this.courselist = [...this.originalCourselist];
-    this.filteredCount = this.courselist.length;
-    this.extractUniqueSubjects(); // Uppdatera unika ämnen till att inkludera alla ämnen
-}
-
-
-  //function to sort courses based on selected headline
-  sortCourses(column: keyof CourseInterface): void {
-    this.courselist.sort((a, b) => {
-      const valueA = a[column];
-      const valueB = b[column];
-      const sortOrder = this.isSortAscending ? 1 : -1;
-
-      if (valueA < valueB) {
-        return -1 * sortOrder;
-      } else if (valueA > valueB) {
-        return 1 * sortOrder;
-      } else {
-        return 0;
-      }
-    });
-
-    this.courselist.reverse();
-    this.isSortAscending = !this.isSortAscending;
-  }
-
-  //call addToMyCourses from timetable service
-  addToMyCourses(course: CourseInterface): void {
-    this.timeTableService.addToMyCourses(course);
-  }
-  extractUniqueSubjects(): void {
-    this.uniqueSubjects = Array.from(new Set(this.courselist.map(course => course.subject)));
-  }
-
-  updateDisplayedCourses(): void {
-    this.courselist = [...this.originalCourselist];
-    this.filteredCount = this.courselist.length;
-    this.extractUniqueSubjects();
-  }
+  
 
 }
