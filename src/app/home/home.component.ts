43dbe27e802f6f4163d37e675ad34a7374f5865f
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2024-05-14 12:48:24 +0200

    fått paginering att fungera

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,18 @@
+
 import { Component } from '@angular/core';
 import { CourseInterface } from '../model/course-interface';
 import { CoursedataService } from '../services/coursedata.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { TimetableService } from '../services/timetable.service';
+import { NgxPaginationModule } from 'ngx-pagination';
+
+ 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, NgxPaginationModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -21,6 +25,8 @@ export class HomeComponent {
   searchInput: string = "";//empty string for searchinput
   currentSortColumn: keyof CourseInterface = "courseCode"; //starting with courseCode sorted
   isSortAscending: boolean = true;//sortingdirection  
+  totalLength: number = 0;
+  page: number = 1;//start on page one
 
 
   constructor(
--- src/app/services/coursedata.service.ts
+++ src/app/services/coursedata.service.ts
@@ -9,10 +9,10 @@ import { CourseInterface } from '../model/course-interface';
 export class CoursedataService {
   private url:string = "https://matdah.github.io/DT208G---Programmering-i-TypeScript/Moment%205%20-%20Projekt/miun_courses.json";
   
-  //dependencie injection http client
+  //dependency injection av HttpClient
   constructor(private http: HttpClient) { } 
   
-  //method to get courses with http client
+  //method to get courses using HttpClient
   getCourses(): Observable<CourseInterface[]> {
     return this.http.get<CourseInterface[]>(this.url);
   }
